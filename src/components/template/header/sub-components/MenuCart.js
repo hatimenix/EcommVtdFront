0a3a37dd4ad9cf9944bf2dc80886919940cf95fe
PaiperleckMaroc <dev.maroc@paiperleck.lu>
2023-09-12 14:50:38 +0100

    avant pull alio

--- src/components/template/header/sub-components/MenuCart.js
+++ src/components/template/header/sub-components/MenuCart.js
@@ -89,18 +89,18 @@ const MenuCart = () => {
           </div>
           <div className="shopping-cart-btn btn-hover text-center">
             <Link className="default-btn" to={process.env.PUBLIC_URL + "/cart"}>
-              view cart
+              Voir le panier
             </Link>
             <Link
               className="default-btn"
               to={process.env.PUBLIC_URL + "/checkout"}
             >
-              checkout
+              Passer la commande
             </Link>
           </div>
         </Fragment>
       ) : (
-        <p className="text-center">No items added to cart</p>
+        <p className="text-center">pas d'article dans le panier</p>
       )}
     </div>
   );
