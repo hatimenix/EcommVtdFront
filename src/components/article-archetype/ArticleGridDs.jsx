fbc6f7d82dacff9ad623b5939a13343c8bdf99df
HatimAbarki <dev6@paiperleck.lu>
2023-09-19 10:13:51 +0100

    articles display __v2.10______

--- src/components/article-archetype/ArticleGridDs.jsx
+++ src/components/article-archetype/ArticleGridDs.jsx
@@ -1,310 +1,87 @@
 import PropTypes from "prop-types";
-
 import SectionTitle from "../SectionTitle";
-import { resetSelectedCategory, selectCategory, setCategories } from "../../store/slices/categoriesSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { setArticles, setLoadedArticles } from "../../store/slices/articlesSlice";
 import { useEffect, useState } from "react";
-import { fetchArticles, fetchArticlesByCategory, fetchCategories, fetchCst, fetchSellers } from "../../services/fetchData";
-import FeatureIconTwo from "../../wrappers/feature-icon/FeatureIconTwo";
+import { fetchCst } from "../../services/fetchData";
 import ArticleGridDsTwo from "./ArticleGridDsTwo";
-import HeroSliderTen from "../../wrappers/hero-slider/HeroSliderTen";
-import { useLocation } from "react-router-dom";
-import { fetchUser, setUser } from "../../store/slices/userSlice";
-import { useArticleSelector, useCategorySelector, useCurrentUserSelector, usePropsSelectore, useRecSelector } from "../../store/selectors/selectors";
-import ArticleMarqueGrid from "./articles-marque/ArticleMarqueGrid";
+import { fetchUser } from "../../store/slices/userSlice";
 
 const ArticleGridDs = ({ limit }) => {
-
-
-
-
-
-
     const dispatch = useDispatch();
+    const initialDisplayedArticles = 10; // Initial number of articles to display
+    const [displayedArticles, setDisplayedArticles] = useState(
+        () => parseInt(localStorage.getItem("displayedArticles")) || initialDisplayedArticles
+    );
 
-
-
-
-
-
-
-    const categories = useSelector((state) => state.categorie.categories);
-    const selectedCategory = useSelector((state) => state.categorie.selectedCategory);
-    const articles = useSelector((state) => state.article.articles);
     const iArticles = useSelector((state) => state.article.articles);
     const iCategories = useSelector((state) => state.categorie.categories);
-
-    const __articlesRec = useArticleSelector();
-    // const __categoriesRec = useCategorySelector();
-
-    const __recs = useRecSelector()
-
-
-    const __props = usePropsSelectore()
-
-
-
-
-
-
-
-
-    console.log("props", __props);
-
-    const uniqueArticles = new Set(); // Create a Set to store unique articles
-    const targetedCategories = new Set(); // Create a Set to store unique targeted categories
-
-    // If __recs is an array
-    if (Array.isArray(__recs)) {
-        __recs.forEach(rec => {
-            __props.forEach(prop => {
-
-                const correspondingArticle = __articlesRec.find(article => (article.id_art === rec.article) || (article.id_art === prop.article));
-
-                if (correspondingArticle) {
-                    // Add the article's id or a unique identifier to the Set
-                    uniqueArticles.add(correspondingArticle.id_art);
-                    // Add the article's category to the targetedCategories Set
-                    targetedCategories.add(correspondingArticle.categorie_id);
-                }
-            });
-        })
-    } else {
-        console.log('Invalid __recs data.');
-    }
-
-    
-
-
-
-
-    // Convert the Set back to an array of unique articles
-    const targetedArticles = Array.from(uniqueArticles).map(id_art => {
-        return __articlesRec.find(article => article.id_art === id_art);
-    });
-
-    // Now, you can filter articles from __articlesRec based on the targetedCategories
-    const articlesWithSameCategory = __articlesRec.filter(article => {
-        return targetedCategories.has(article.categorie_id);
-    });
-
-
-    console.log("articlesWithSameCategory", articlesWithSameCategory);
-
-    // const correspondingArticle = __articlesRec.find(article => article.id_art === __recs.article);
-
-    // console.log('art', correspondingArticle);
     const currentuser = useSelector((state) => state.user);
+    const csts = useSelector((state) => state.cst.csts);
 
     useEffect(() => {
         dispatch(fetchUser());
+        dispatch(fetchCst());
     }, [dispatch]);
 
-    const userHasTargetedArticles = targetedArticles.length > 0;
-    const userHasClickedOnArticle = Array.isArray(__recs) && __recs.some(rec => rec.Customer === currentuser.id);
-
-
-
-    const csts = useSelector((state) => state.cst.csts);
-    const location = useLocation();
-
-
-
-    const str = location.pathname;
-    const arr = str.split('/');
-    const lastSubstring = arr[arr.length - 1];
-    const lastCharacter = parseInt(lastSubstring.slice(-1));
-
-
-
-
     useEffect(() => {
-        // Fetch CST data
-        dispatch(fetchCst());
+        const isInfiniteScrollEnabled = localStorage.getItem("infiniteScrollEnabled") === "true";
 
-
-        if (str === '/') {
-            localStorage.removeItem('selectedCategory');
-            dispatch(resetSelectedCategory()); // Reset the selected category in the store as well
-        } else
-
-            if (lastCharacter) {
-                const isDifferentCategory = !selectedCategory || selectedCategory.id_cat !== lastCharacter;
-
-                if (isDifferentCategory) {
-                    dispatch(fetchArticlesByCategory(lastCharacter));
-                    dispatch(selectCategory({ id_cat: lastCharacter }));
-                }
-            }
-
-
-    }, [dispatch, selectedCategory, lastCharacter]);
-
-
-
-
-    const category = dispatch(selectCategory(selectedCategory));
-    let catId = 0
-
-    if (category.payload == null) {
-        catId = null;
-    } else {
-        catId = category.payload.id_cat
-    }
-
-
-
-
-    const selectedCat = categories.find((category) => category.id_cat === lastCharacter || category.id_cat === catId);
-
-
-    const cLoc = useLocation()
-    const cRoad = cLoc.pathname
-    const [catTitle, setCatTitle] = useState("");
-
-
-
-
-    useEffect(() => {
-        if (selectedCat == undefined) {
-            setCatTitle("Catégorie Undefined ")
-        } else {
-            setCatTitle(selectedCat.titre)
+        if (isInfiniteScrollEnabled) {
+            // Add a scroll event listener to trigger loadMoreArticles
+            window.addEventListener("scroll", handleScroll);
         }
-    }, [selectedCat]);
-    console.log(catTitle);
 
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [displayedArticles]);
 
+    const handleScroll = () => {
+        if (
+            window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+            displayedArticles < iArticles.length
+        ) {
+            // When the user has scrolled near the bottom and there are more articles to load
+            setDisplayedArticles(displayedArticles + 10);
+        }
+    };
 
+    const toggleInfiniteScroll = () => {
+        // Toggle the infinite scroll flag in localStorage
+        const isInfiniteScrollEnabled = localStorage.getItem("infiniteScrollEnabled") === "true";
+        localStorage.setItem("infiniteScrollEnabled", !isInfiniteScrollEnabled);
 
-    // console.log('cuser', userData);
+        // If infinite scroll is enabled, add the scroll event listener
+        if (!isInfiniteScrollEnabled) {
+            window.addEventListener("scroll", handleScroll);
+        } else {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    };
 
-
-    // console.log('current user status ', currentUser.is_seller);
-
-
-    console.log('TargetedARticles ', targetedArticles);
-
-
-
-    const yourProp = currentuser.first_name; // Replace with your actual prop value
-    const yourPayload = [/* Your payload data here */]; // Replace with your payload
-
-    dispatch(setUser(yourProp, yourPayload));
-
-
-
-    localStorage.getItem("number")
-
+    const displayedArticleList = iArticles.slice(0, displayedArticles);
 
     return (
         <div className="product-area pb-60 section-padding-1">
             <div className="container-fluid">
-                {cRoad !== '/' ?
-                    <>
-                        <SectionTitle
-                            titleText={catTitle}
-                            // subTitleText="Latest arrivals & offers "
-                            // positionClass="text-center"
-                            spaceClass="mb-20 mt-40"
-                        />
-                        <div className="row five-column">
-                            <ArticleGridDsTwo
-                                articles={articles}
-                                categories={categories}
-                                csts={csts}
-                                selectedCategory={selectedCategory}
-                                limit={limit}
-                                spaceBottomClass="mb-25"
-                            />
-                        </div>
-                    </>
-
-                    : <></>
-
-                }
-
-
-
-                {/* <FeatureIconTwo spaceTopClass="pt-70" spaceBottomClass="pb-60" /> */}
-
-
-                {true ?
-
-
-                    <>
-                        {userHasTargetedArticles && userHasClickedOnArticle && (
-                            <>
-                                <SectionTitle
-                                    titleText="Recommandé pour toi"
-                                    // subTitleText="Latest arrivals & offers "
-                                    // positionClass="text-center"
-                                    spaceClass="mb-20 mt-80"
-                                />
-                                <div className="row five-column">
-                                    <ArticleGridDsTwo
-                                        articles={articlesWithSameCategory}
-                                        // categories={iCategories}
-                                        csts={csts}
-                                        limit={limit}
-                                        spaceBottomClass="mb-25"
-                                    />
-                                </div>
-
-                            </>
-                        )}
-
-
-
-
-                        <SectionTitle
-                            titleText="Recherche par marque"
-                            // subTitleText="Latest arrivals & offers "
-                            // positionClass="text-center"
-                            spaceClass="mb-20 mt-30"
-                        />
-
-                        <div className="row five-column">
-                            <ArticleMarqueGrid />
-                        </div>
-
-
-                        <SectionTitle
-                            titleText="Fil d'actu"
-                            // subTitleText="Latest arrivals & offers "
-                            // positionClass="text-center"
-                            spaceClass="mb-50 mt-30"
-                        />
-
-                        <div className="row five-column">
-                            <ArticleGridDsTwo
-                                articles={iArticles}
-                                categories={iCategories}
-                                csts={csts}
-                                limit={limit}
-                                spaceBottomClass="mb-25 "
-                            />
-                        </div>
-
-
-
-                    </>
-                    : <p>ijsi</p>
-
-
-
-
-
-                }
+                <SectionTitle titleText="Fil d'actu" spaceClass="mb-50 mt-30" />
+                <button onClick={toggleInfiniteScroll}>Toggle Infinite Scroll</button>
+                <div className="row five-column">
+                    <ArticleGridDsTwo
+                        articles={displayedArticleList}
+                        categories={iCategories}
+                        csts={csts}
+                        limit={limit}
+                        spaceBottomClass="mb-25"
+                    />
+                </div>
             </div>
         </div>
     );
 };
 
 ArticleGridDs.propTypes = {
-
-    limit: PropTypes.number
+    limit: PropTypes.number,
 };
 
 export default ArticleGridDs;
